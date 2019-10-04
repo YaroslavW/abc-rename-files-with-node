015436711caf3eeecc59100db3ed50f6ac10a453
Yaroslav <doctor355@yahoo.com>
2019-10-04 10:49:21 +0300

    renaming files using path and saving extension

--- rename.js
+++ rename.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
-const srcFolder = "C:/Users/Branch/Node/abc-rename-file/srcImage/";
-const outFolder = "C:/Users/GoogleD/outFolder/";
+var path = require("path");
+
+const srcFolder = path.resolve(__dirname, "srcImage") + "/";
+const outFolder = path.resolve(__dirname, "outImage") + "/";
 
 var d = new Date();
 var curr_date = d.getDate();
@@ -8,14 +10,15 @@ var curr_month = d.getMonth() + 1;
 var curr_year = d.getFullYear();
 
 var data_f = curr_date + "-" + curr_month + "-" + curr_year;
-var myPrefix = "y";
+var myPrefix = "-tr1-";
 var prefix = myPrefix + data_f;
 
 fs.readdir(srcFolder, (err, files) => {
   files.forEach((file, i) => {
-    fs.rename(srcFolder + file, outFolder + i + prefix + ".jpg", err => {
+    let extension = path.extname(file);
+    fs.rename(srcFolder + file, outFolder + i + prefix + extension, err => {
       if (err) throw err;
-      console.log("rename completed!");
+      console.log(`rename - ${file} completed!`);
     });
   });
 });
